6dac58e14beff2c4cb4c6f7c7b0465db3def71a4
TheFrenchGuy <noedelacroix@gmail.com>
2021-07-21 14:44:46 +0200

    Initial Commit

---
+++ ToDoToday/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoToday
+//
+//  Created by Noe De La Croix on 21/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDoToday/ToDoTodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoTodayApp.swift
+//  ToDoToday
+//
+//  Created by Noe De La Croix on 21/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct ToDoTodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
